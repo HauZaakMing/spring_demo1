d42a38bf72ca968b9039adcf14abf7860f1c0529
HauZaakMing <houzeming123@163.com>
2023-03-14 15:01:51 +0800

    2023/3/14

--- spring6-ioc-Annotation/src/main/java/org/example/Main.java
+++ spring6-ioc-Annotation/src/main/java/org/example/Main.java
@@ -1,12 +1,15 @@
 package org.example;
 
+import org.example.config.SpringConfig;
 import org.example.controller.UserController;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
+        ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
         UserController userController = context.getBean("userController", UserController.class);
         userController.add_userController();
 
